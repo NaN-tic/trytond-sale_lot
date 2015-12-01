c6b4ad11b2c7aa414547b84b5a0208c92ade6941
resteve <resteve@zikzakmedia.com>
2015-12-01 09:43:55 +0100

    Tests

--- tests/test_sale_lot.py
+++ tests/test_sale_lot.py
@@ -1,27 +1,18 @@
-#!/usr/bin/env python
+# This file is part of the sale_lot module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class TestCase(unittest.TestCase):
-    'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('sale_lot')
-
-    def test0005views(self):
-        'Test views'
-        test_view('sale_lot')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class SaleLotTestCase(ModuleTestCase):
+    'Test Sale Lot module'
+    module = 'sale_lot'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+        SaleLotTestCase))
     return suite
